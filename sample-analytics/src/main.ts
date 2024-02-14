de4bcdb5254f1ae4cba204ac4bb9f31e89b4d254
sarwirr <sarrah.benhalima@insat.ucar.tn>
2024-02-14 10:57:04 +0100

    fixed conf for sample-analytics

--- sample-analytics/src/main.ts
+++ sample-analytics/src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
-      port: 3002
-    }
+      port: 3002,
+    },
   });
-  await app.startAllMicroservices();
-  await app.listen(3002);
+  await app.listen();
 }
 bootstrap();
